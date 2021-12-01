f1ffa1144f52e90a246ed8b54de95570bd285627
ffraga98 <ferfraga2@gmail.com>
2021-12-01 17:40:32 -0300

    Agrego disminuir y aumentar construidos. Cmabio las flechas por legibilidad.

--- edificio/Edificio.cpp
+++ edificio/Edificio.cpp
@@ -2,37 +2,37 @@
 #include <iostream>
 
 Edificio::Edificio() {
-	this -> nombre = EDIFICIO_VACIO;
-	this -> materiales[0] = 0;
-	this -> materiales[1] = 0;
-	this -> materiales[2] = 0;
-	this -> maximo_permitidos = 0;
-	this -> construidos = 0;
+	this->nombre = EDIFICIO_VACIO;
+	this->materiales[0] = 0;
+	this->materiales[1] = 0;
+	this->materiales[2] = 0;
+	this->maximo_permitidos = 0;
+	this->construidos = 0;
 }
 
-Edificio::Edificio(string nombre, int piedra, int madera, int metal, int maximo_permitidos){
-	this -> nombre = nombre;
-	this -> materiales[PIEDRA] = piedra;
-	this -> materiales[MADERA] = madera;
-	this -> materiales[METAL] = metal;
-	this -> maximo_permitidos = maximo_permitidos;
-	this -> construidos = 0;
+Edificio::Edificio(string nombre, std::size_t piedra, std::size_t madera, std::size_t metal, std::size_t maximo_permitidos){
+	this->nombre = nombre;
+	this->materiales[PIEDRA] = piedra;
+	this->materiales[MADERA] = madera;
+	this->materiales[METAL] = metal;
+	this->maximo_permitidos = maximo_permitidos;
+	this->construidos = 0;
 }
 
 Edificio::~Edificio(){
 
 }
 
-string Edificio::obtener_nombre(){
-	return this -> nombre;
+string Edificio::obtener_nombre() const {
+	return this->nombre;
 }
 
-int Edificio::obtener_cant_material(string material){
-	int cantidad = 0, i = 0;
+std::size_t Edificio::obtener_cant_material(string material){
+	std::size_t cantidad = 0, i = 0;
 	bool fin = false;
 	while(i < CANT_MATERIALES_EDIFICIOS && !fin){
 		if(MATERTIALES_EDIFICIOS[i] == material){
-			cantidad = this -> materiales[i];
+			cantidad = this->materiales[i];
 			fin = true;
 		}
 		i++;
@@ -40,23 +40,43 @@ int Edificio::obtener_cant_material(string material){
 	return cantidad;
 }
 
-int Edificio::obtener_max_permitidos(){
-	return this -> maximo_permitidos;
+std::size_t Edificio::obtener_max_permitidos(){
+	return this->maximo_permitidos;
 }
 
 /*void Edificio::saludar(){
-	cout << "Soy un/a " << this -> nombre << " y estoy en el casillero consultado." << endl;
+	cout << "Soy un/a " << this->nombre << " y estoy en el casillero consultado." << endl;
 } */
 
 bool Edificio::vacio(){
-	return this -> nombre == EDIFICIO_VACIO;
+	return this->nombre == EDIFICIO_VACIO;
 }
 
-void Edificio::modificar_construidos(int n){
-	this -> construidos = n;
+void Edificio::modificar_construidos(std::size_t n){
+	if(n < this->maximo_permitidos)
+		this->construidos = n;
 }
 
-int Edificio::obtener_construidos(){
-	return this -> construidos;
+void Edificio::aumentar_construidos(){
+	this->modificar_construidos(construidos + 1);
+
 }
 
+void Edificio::disminuir_construidos(){
+	this->modificar_construidos(construidos - 1);	
+}
+
+
+std::size_t Edificio::obtener_construidos(){
+	return this->construidos;
+}
+
+/* Por ahora, inutiles
+bool Edificio::operator==(const Edificio &rhs){
+	return this->nombre == rhs.obtener_nombre();
+}
+
+bool Edificio::operator!=(const Edificio &rhs){
+	return this->nombre != rhs.obtener_nombre();
+}
+*/

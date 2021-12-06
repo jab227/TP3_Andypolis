5a63dafe32527f9aa927c8d194c0a6bb76cd2d5d
Juan <jaguirreb@fi.uba.ar>
2021-12-06 04:04:02 -0300

    Removi func, la responsabilidad paso al parser

--- utils/LecturaArchivos.cpp
+++ utils/LecturaArchivos.cpp
@@ -1,129 +1,53 @@
 #include "LecturaArchivos.h"
 
-#include <ctype.h>
-#include "../edificio/edificios/Mina.h"
-#include "../edificio/edificios/Aserradero.h"
-#include "../edificio/edificios/Fabrica.h"
-#include "../edificio/edificios/Escuela.h"
-#include "../edificio/edificios/Obelisco.h"
-#include "../edificio/edificios/PlantaElectrica.h"
-#include "../Casillero/CasilleroTransitable.h"
 #include "../Casillero/CasilleroConstruible.h"
 #include "../Casillero/CasilleroInaccesible.h"
+#include "../Casillero/CasilleroTransitable.h"
+#include "../edificio/edificios/Aserradero.h"
+#include "../edificio/edificios/Escuela.h"
+#include "../edificio/edificios/Fabrica.h"
+#include "../edificio/edificios/Mina.h"
+#include "../edificio/edificios/Obelisco.h"
+#include "../edificio/edificios/PlantaElectrica.h"
 
-//PRE: -
-//POST: parte la linea en el delimitador, devuelve la primera mitad por la salida y la segunda mitad
-//se iguala a linea
-string dividir_linea(string &linea, char delimitador);
-
-//PRE: c debe ser el caracter correspondiente a un numero
-//POST: devuelve el numero entero
-std::size_t char_a_int(char c);
-
-bool es_numero(std::string palabra){
-	bool resultado = true;
-	std::size_t i = 0;
-	if(palabra.size() == 0)
-		resultado = false;
-	while (i < palabra.length() && resultado){
-		resultado = isdigit(palabra[i]);
-		i++;
-	}
-	return resultado;
-}
-
-string dividir_linea(string &linea, char delimitador){
-	std::size_t pos = linea.find(delimitador);
-	string dato = linea.substr(0, pos);
-	linea.erase(0, pos+1);
-	return dato;
-}
-
-Edificio* procesar_edificio(string linea){
-	string nombre = dividir_linea(linea, ESPACIO);
-	string dato_sig;
-	while(!es_numero(dato_sig = dividir_linea(linea, ESPACIO))){
-		nombre += ' ';
-		nombre += dato_sig;
-	}
-	std::size_t materiales[CANT_MATERIALES_EDIFICIOS];
-	materiales[0] = stoi(dato_sig);
-	for (std::size_t j = 1; j < CANT_MATERIALES_EDIFICIOS; j++){
-		materiales[j] = stoi(dividir_linea(linea, ESPACIO));
-	}
-	std::size_t max_permitidos = stoi(linea);
-	return traductor_edificios(nombre, materiales[0], materiales[1], materiales[2], max_permitidos, NINGUNO);
-}
-
-Edificio* traductor_edificios(string nombre, std::size_t piedra, std::size_t madera, std::size_t metal, std::size_t max_permitidos, std::size_t propietario){
+Edificio* traductor_edificios(std::string nombre, std::size_t piedra,
+			      std::size_t madera, std::size_t metal,
+			      std::size_t max_permitidos) {
 	Edificio* edificio = nullptr;
-	if(nombre == "mina")
-		edificio = new Mina(piedra, madera, metal, max_permitidos, propietario);
+	if (nombre == "mina")
+		edificio = new Mina(piedra, madera, metal, max_permitidos);
 	else if (nombre == "aserradero")
-		edificio = new Aserradero(piedra, madera, metal, max_permitidos, propietario);
-	else if(nombre == "fabrica")
-		edificio = new Fabrica(piedra, madera, metal, max_permitidos, propietario);
-	else if(nombre == "escuela")
-		edificio = new Escuela(piedra, madera, metal, max_permitidos, propietario);
-	else if(nombre == "obelisco")
-		edificio = new Obelisco(piedra, madera, metal, max_permitidos, propietario);
-	else if(nombre == "planta electrica")
-		edificio = new Planta_Electrica(piedra, madera, metal, max_permitidos, propietario);
+		edificio =
+		    new Aserradero(piedra, madera, metal, max_permitidos);
+	else if (nombre == "fabrica")
+		edificio = new Fabrica(piedra, madera, metal, max_permitidos);
+	else if (nombre == "escuela")
+		edificio = new Escuela(piedra, madera, metal, max_permitidos);
+	else if (nombre == "obelisco")
+		edificio = new Obelisco(piedra, madera, metal, max_permitidos);
+	else if (nombre == "planta electrica")
+		edificio =
+		    new Planta_Electrica(piedra, madera, metal, max_permitidos);
 	return edificio;
 }
 
-Casillero* traductor_casillero(char nombre){
-    Casillero* casillero;
-    if(nombre == TRANSITABLE)
-        casillero = new Casillero_Transitable();
-    else if (nombre == INACCESIBLE)
-        casillero = new Casillero_Inaccesible();
-    else if(nombre == CONSTRUIBLE)
-        casillero = new Casillero_Construible();
-    return casillero;
+Casillero* traductor_casillero(char nombre) {
+	Casillero* casillero;
+	if (nombre == TRANSITABLE)
+		casillero = new Casillero_Transitable();
+	else if (nombre == INACCESIBLE)
+		casillero = new Casillero_Inaccesible();
+	else if (nombre == CONSTRUIBLE)
+		casillero = new Casillero_Construible();
+	return casillero;
 }
 
-Material* traductor_materiales(string nombre, std::size_t cantidad){
+Material* traductor_materiales(std::string nombre, std::size_t cantidad) {
 	return new Material(nombre, cantidad);
 }
 
-Material procesar_material(string linea){
-	string nombre = dividir_linea(linea, ESPACIO);
-	string dato_sig;
-	while(!es_numero(dato_sig = dividir_linea(linea, ESPACIO))){
-		nombre += ' ';
-		nombre += dato_sig;
-	}
-	std::size_t cantidad = stoi(dato_sig);
-	return Material(nombre, cantidad);
-}
-
-void cargar_fila_columna(string linea, std::size_t& filas, std::size_t& columnas){
+void cargar_fila_columna(std::string linea, std::size_t& filas,
+			 std::size_t& columnas) {
 	filas = stoi(dividir_linea(linea, ESPACIO));
 	columnas = stoi(dividir_linea(linea, ESPACIO));
 }
-
-string procesar_ubicacion(string linea, std::size_t &fila, std::size_t &columna, size_t &propietario){
-	string edificio = dividir_linea(linea, DELIMITADOR_UBICACION);
-	edificio.erase(edificio.size()-1, 1); //Le saco el espcio final
-	fila = char_a_int(linea[0]);
-	columna = char_a_int(linea[3]);
-	propietario = 1;					//Elegir bien el propietario
-	return edificio;
-}
-
-std::size_t char_a_int(char c){
-	return (c - '0');
-}
-
-string espaciado(string palabra, std::size_t largo){
-	string espacio = "";
-	std::size_t largo_final = palabra.size();
-	while(largo_final < largo){
-		espacio += "\t";
-		largo_final += 7;
-	}
-	espacio += "|";
-	return espacio;
-}
-
--- utils/LecturaArchivos.h
+++ utils/LecturaArchivos.h
@@ -2,49 +2,35 @@
 #define LECTURAARCHIVOS_H_
 
 #include <string>
-#include "../edificio/Edificio.h"
+
 #include "../Casillero/Casillero.h"
+#include "../edificio/Edificio.h"
 
-using namespace std;
+// PRE: linea debe tener el formato valido para el rango del mapa
+// POST: se carga la linea y la fila con los valores correspondientes a la linea
+// ingresada
+void cargar_fila_columna(std::string linea, std::size_t& filas,
+			 std::size_t& columnas);
 
-const char ESPACIO = ' ', ENTER = 10, DELIMITADOR_UBICACION = '(';
+// PRE: nombre debe ser un edificio valido
+// POST: devuelve el edificio cargado con los datos
+Edificio* traductor_edificios(std::string nombre, std::size_t piedra,
+			      std::size_t madera, std::size_t metal,
+			      std::size_t max_permitidos);
 
-//PRE: -
-//POST: devuelve si la palabra esta compuesta por todos numeros o no.
-bool es_numero(string palabra);
+// PRE: nombre debe ser un edificio valido
+// POST: devuelve el edificio cargado con los datos
+Edificio* traductor_edificios(std::string nombre, std::size_t piedra,
+			      std::size_t madera, std::size_t metal,
+			      std::size_t max_permitidos, size_t propietario);
 
-//PRE: linea debe tener el formato valido para los edificios
-//POST: se procesa la linea y se devuelve el edificio referencido en la linea.
-Edificio* procesar_edificio(string linea);
-
-//PRE: linea debe tener el formato valido para los materiales
-//POST: se procesa la linea y se devuelve el material referencido en la linea.
-Material procesar_material(string linea);
-
-//PRE: linea debe tener el formato valido para el rango del mapa
-//POST: se carga la linea y la fila con los valores correspondientes a la linea ingresada
-void cargar_fila_columna(string linea, std::size_t &filas, std::size_t &columnas);
-
-//PRE: nombre debe ser un edificio valido
-//POST: devuelve el edificio cargado con los datos
-Edificio* traductor_edificios(string nombre, std::size_t piedra, std::size_t madera, std::size_t metal, std::size_t max_permitidos);
-
-string procesar_ubicacion(string linea, std::size_t &fila, std::size_t &columna, size_t &propietario);
-
-//PRE: nombre debe ser un edificio valido
-//POST: devuelve el edificio cargado con los datos
-Edificio* traductor_edificios(string nombre, std::size_t piedra, std::size_t madera, std::size_t metal, std::size_t max_permitidos, size_t propietario);
- 
-//PRE: nombre debe corresponder a un casillero
-//POST: se devuelve el casillero vacio correspondiente a nombre creado en memoria dinamica
+// PRE: nombre debe corresponder a un casillero
+// POST: se devuelve el casillero vacio correspondiente a nombre creado en
+// memoria dinamica
 Casillero* traductor_casillero(char nombre);
 
-//PRE: nombre debe ser un material valido
-//POST: devuelve el material cargado con los datos
-Material* traductor_materiales(string nombre, std::size_t cantidad);
-
-//PRE: -
-//POST: devuelve un espaciado correspondiente al laro deseado y al largo de la palabra
-string espaciado(string palabra, std::size_t largo);
+// PRE: nombre debe ser un material valido
+// POST: devuelve el material cargado con los datos
+Material* traductor_materiales(std::string nombre, std::size_t cantidad);
 
 #endif /* LECTURAARCHIVOS_H_ */

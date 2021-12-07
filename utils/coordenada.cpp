767882e34a112f37859547440fbd2574b3285551
ffraga98 <ferfraga2@gmail.com>
2021-12-07 01:33:12 -0300

    Coordenada testeado y documentado.

--- utils/coordenada.cpp
+++ utils/coordenada.cpp
@@ -2,9 +2,11 @@
 
 
 Coordenada::Coordenada(const std::size_t& x, const std::size_t& y) : x_(x), y_(y) {}
+
 std::size_t Coordenada::x() const {
 	return x_;
 }
+
 std::size_t Coordenada::y() const {
 	return y_;
 }
@@ -12,9 +14,11 @@ std::size_t Coordenada::y() const {
 std::string Coordenada::a_string() const {
 	return "(" + std::to_string(x()) + ", " + std::to_string(y()) + ")";
 }
+
 bool Coordenada::operator==(const Coordenada& rhs) const {
 	return (x() == rhs.x() && y() == rhs.y());
 }
+
 bool Coordenada::operator!=(const Coordenada& rhs) const {
 	return !(*this == rhs);
 }
--- utils/coordenada.h
+++ utils/coordenada.h
@@ -9,15 +9,41 @@ class Coordenada {
 	std::size_t y_;
 
        public:
+	//Constructor por default
 	Coordenada() = default;
+
+	//Constructor de copia por default
 	Coordenada(const Coordenada& rhs) = default;
+
+	//Operador = por default
 	Coordenada& operator=(const Coordenada& rhs) = default;
+
+	//Destructor por default
 	~Coordenada() = default;
+
+	//PRE: -
+	//POS: Construye una Coordenada con x_ = x e y_ = y
 	Coordenada(const std::size_t& x, const std::size_t& y);
+
+	//PRE: rhs debe ser una instancia de Coordenada existente.
+	//POS: Sobrecarga del operador ==. True si x_ e y_ son iguales a las de rhs.
 	bool operator==(const Coordenada& rhs) const;
+
+	//PRE: rhs debe ser una instancia de Coordenada existente.
+	//POS: Sobrecarga del operador !=. True si x_ e y_ son distintas a las de rhs.
 	bool operator!=(const Coordenada& rhs) const;
+
+	//PRE: -
+	//POS: Devuelve x_
 	std::size_t x() const;
+
+	//PRE: -
+	//POS: Devuelve y_
 	std::size_t y() const;
+
+	//PRE: -
+	//POS: Retorna el string (x_, y_). (Como en el formato ubicaciones.txt)
 	std::string a_string() const;
+
 };
 #endif

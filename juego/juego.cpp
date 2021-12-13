2e7f0fa9bf9e0daad975d4d549e764d9b8867b64
Juan <jaguirreb@fi.uba.ar>
2021-12-13 05:11:12 -0300

    FIX: linting

--- juego/juego.cpp
+++ juego/juego.cpp
@@ -1,38 +1,43 @@
 #include "juego.h"
 
-Juego::Juego() : programa_(Programa(RUTA_MATERIALES, RUTA_EDIFICIOS, 
-                        RUTA_MAPA, RUTA_UBICACIONES)) {}
+Juego::Juego() : programa_(Programa(RUTA_MATERIALES, RUTA_EDIFICIOS,
+                                    RUTA_MAPA, RUTA_UBICACIONES)) {}
 
 Salida Juego::comenzar_juego() {
-        mensaje_bienvenida();
-        std::size_t opcion;
-        Salida salida;
-        do {
-            programa_.mostrar_menu_inicio();
-            opcion = pedir_opcion_valida([&](std::string opcion) { 
-                    return programa_.es_opcion_valida_inicio(opcion); });
-            programa_.limpiar_pantalla();
-            salida = programa_.procesar_opcion_inicio(opcion);
-        } while(salida != GUARDAR_Y_SALIR && salida != COMENZAR_JUEGO);
-        return salida;
+    mensaje_bienvenida();
+    std::size_t opcion;
+    Salida salida;
+    do
+    {
+        programa_.mostrar_menu_inicio();
+        opcion = pedir_opcion_valida([&](std::string opcion)
+                                     { return programa_.es_opcion_valida_inicio(opcion); });
+        programa_.limpiar_pantalla();
+        salida = programa_.procesar_opcion_inicio(opcion);
+    } while (salida != GUARDAR_Y_SALIR && salida != COMENZAR_JUEGO);
+    return salida;
 }
 
 Salida Juego::jugar() {
     std::size_t opcion;
     Salida salida;
-    if (!programa_.victoria()) { 
-        do {
+    if (!programa_.victoria())
+    {
+        do
+        {
             programa_.mostrar_menu_juego();
             opcion = pedir_opcion_valida(
-                [&](std::string opcion) { 
-                    return programa_.es_opcion_valida_juego(opcion); });
+                [&](std::string opcion)
+                { return programa_.es_opcion_valida_juego(opcion); });
             programa_.limpiar_pantalla();
-            if(programa_.es_energia_suficiente(opcion))
+            if (programa_.es_energia_suficiente(opcion))
                 salida = programa_.procesar_opcion_juego(opcion);
-            else   
+            else
                 energia_insuficiente();
-        } while(salida != GUARDAR_Y_SALIR && salida != VICTORIA);
-    } else {
+        } while (salida != GUARDAR_Y_SALIR && salida != VICTORIA);
+    }
+    else
+    {
         salida = VICTORIA;
     }
     return salida;
@@ -42,14 +47,14 @@ void Juego::finalizar_juego(Salida salida) {
     programa_.limpiar_pantalla();
     if (salida == VICTORIA)
         mensaje_victoria();
-    programa_.guardar_archivos(RUTA_UBICACIONES, 
-                            RUTA_MATERIALES, RUTA_EDIFICIOS);
+    programa_.guardar_archivos(RUTA_UBICACIONES,
+                               RUTA_MATERIALES, RUTA_EDIFICIOS);
     mensaje_despedida();
 }
 
 void Juego::mensaje_bienvenida() const {
-	programa_.limpiar_pantalla();
-	cout << "Bienvenido!" << endl;
+    programa_.limpiar_pantalla();
+    cout << "Bienvenido!" << endl;
 }
 
 void Juego::energia_insuficiente() const {
@@ -60,6 +65,7 @@ void Juego::mensaje_victoria() const {
     std::cout << "GANASTE" << endl;
 }
 
-void Juego::mensaje_despedida() const {
+void Juego::mensaje_despedida() const
+{
     std::cout << "Adios" << std::endl;
 }
